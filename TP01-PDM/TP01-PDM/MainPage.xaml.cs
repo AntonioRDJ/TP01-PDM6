40d694f078840b5bf122975b5f127cd106e56027
Antonio Rodrigues <antonio.rodrdj@gmail.com>
2022-08-28 15:44:27 -0300

    first commit

---
+++ TP01-PDM/TP01-PDM/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TP01_PDM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnClickConfirm(object sender, EventArgs args)
+        {
+            Console.WriteLine("sender ", sender);
+            Console.WriteLine("args ", args);
+
+            if(id.Text.Equals("admin") && pass.Text.Equals("senha@dmin"))
+            {
+                await DisplayAlert("Alerta", "Login realizado com sucesso", "OK");
+            } else
+            {
+                await DisplayAlert("Alerta", "Login não autorizado", "OK");
+            }
+        }
+
+        void OnClickClear(object sender, EventArgs args)
+        {
+            id.Text = "";
+            pass.Text = "";
+            id.Focus();
+        }
+
+        async void OnClickCredits(object sender, EventArgs args)
+        {
+            await DisplayAlert("Autores", "Antonio Rodrigues Dantas Junior \nVitor Hugo Chaves Silva Reis", "OK");
+        }
+    }
+}
